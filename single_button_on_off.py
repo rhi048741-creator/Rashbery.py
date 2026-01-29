0e047ea06efa2e9eac87c849117bea05138fe1a8
rhi048741-creator <rhi048741@gmail.com>
2026-01-29 12:32:12 +0530

    Add files via upload

---
+++ single_button_on_off.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(26,GPIO.IN,pull_up_down=GPIO.PUD_UP )
+count=0
+while True:
+    button=GPIO.input(26)
+    if(button==0):
+        count+=1
+        if(count==1):
+            GPIO.output(22,GPIO.HIGH)
+            GPIO.output(23,GPIO.HIGH)
+            while(GPIO.input(26)==0):
+                pass
+            while(GPIO.input(26)==1):
+                pass
+            count+=1
+        if(count==2):
+            GPIO.output(22,GPIO.LOW)
+            GPIO.output(23,GPIO.LOW)
+            while(GPIO.input(26)==0):
+                pass
+            while(GPIO.input(26)==0):
+                pass
+            count=0
+            
+            
